1985984b6f7373d644339bd299b7ab2e33987770
Calvin Remsburg <cremsburg@protonmail.com>
2023-12-30 17:07:11 -0600

    Add logger utility module

---
+++ src/utils/logger.ts
@@ -0,0 +1,34 @@
+import winston from 'winston';
+
+const { combine, timestamp, json } = winston.format;
+
+const logger = winston.createLogger({
+  level: process.env.LOG_LEVEL || 'info',
+  format: combine(timestamp(), json()),
+  transports: [
+    new winston.transports.File({
+      filename: 'combined.log',
+    }),
+    new winston.transports.File({
+      filename: 'app-error.log',
+      level: 'error',
+    }),
+  ],
+});
+
+//
+// If we're not in production then log to the `console` with the format:
+// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
+//
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.simple(),
+      ),
+    }),
+  );
+}
+
+export default logger;
