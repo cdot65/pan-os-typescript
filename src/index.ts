83a62a58169722e35849c8a4ebab354980eb34ab
Calvin Remsburg <cremsburg@protonmail.com>
2023-12-30 18:04:36 -0600

    Refactor module and class names in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,34 @@
 // src/index.ts
 
 /**
- * @module Provides foundational and specialized classes for interacting with
- * Palo Alto Networks Operating System (PAN-OS) devices including base objects,
- * device-specific classes, and various object types for network configuration.
+ * The root module providing foundational and specialized classes to interact
+ * with Palo Alto Networks Operating System (PAN-OS) devices. It includes base
+ * objects, device-specific classes, and various object types for network configuration.
  */
 
 /**
- * Represents a specialized `PanDevice` for interacting with Palo Alto Networks firewall devices.
- * This class includes methods that provide firewall-specific functionalities,
- * such as resource monitoring and system information retrieval.
+ * A specialized class derived from `PanDevice` for interactions with Palo Alto Networks
+ * firewall devices. It extends the generic device functionalities with methods tailored
+ * to firewall resource monitoring and system information management.
  *
- * @class Firewall
- * @extends PanDevice
- * @exports
+ * @exports Firewall
  */
 export { Firewall } from './models/Firewall';
 
 /**
- * Represents network address configurations in Palo Alto Networks Operating System (PAN-OS).
- * This class provides methods for managing address objects and associating them with
- * security policies and other system entities.
+ * A class representing network address entities (objects) in the context of Palo Alto Networks
+ * Operating System (PAN-OS). It encapsulates the functionality necessary for managing
+ * network address configurations and their associations within security policies and other
+ * system components.
  *
- * @class AddressObject
- * @exports
+ * @exports AddressObject
  */
 export { AddressObject } from './models/AddressObject';
 
 /**
- * Enumerates the supported formats of network addresses in PAN-OS, such as IP netmask,
- * IP range, and Fully Qualified Domain Name (FQDN).
+ * An enumeration of supported network address formats in PAN-OS.
+ * Includes IP netmask, IP range, IP wildcard, and Fully Qualified Domain Name (FQDN) types.
  *
- * @enum AddressType
- * @exports
+ * @exports AddressType
  */
 export { AddressType } from './models/AddressObject';
