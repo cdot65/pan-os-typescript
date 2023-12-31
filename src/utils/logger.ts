79d84f5facbb65203b897ef3b0f51c608be7e41e
Calvin Remsburg <cremsburg@protonmail.com>
2023-12-30 18:02:48 -0600

    Refactor logger configuration for non-production environments

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -2,13 +2,21 @@ import winston from 'winston';
 
 const { combine, timestamp, json } = winston.format;
 
+/**
+ * A configured logger using Winston for application-level logging.
+ * It provides different logging transports based on the runtime environment.
+ * By default, logs 'info' and above level messages. In non-production environments,
+ * it also logs to the console.
+ */
 const logger = winston.createLogger({
   level: process.env.LOG_LEVEL || 'info',
   format: combine(timestamp(), json()),
   transports: [
+    // Transport for persisting general logs in combined.log
     new winston.transports.File({
       filename: 'combined.log',
     }),
+    // Transport for persisting error level logs in app-error.log
     new winston.transports.File({
       filename: 'app-error.log',
       level: 'error',
@@ -16,16 +24,14 @@ const logger = winston.createLogger({
   ],
 });
 
-//
-// If we're not in production then log to the `console` with the format:
-// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
-//
+// Extends the logger configuration for non-production environments.
 if (process.env.NODE_ENV !== 'production') {
+  // In non-production environments, logs are additionally output to the console.
   logger.add(
     new winston.transports.Console({
       format: winston.format.combine(
-        winston.format.colorize(),
-        winston.format.simple(),
+        winston.format.colorize(), // Adds color to the output for improved readability.
+        winston.format.simple(), // Uses a simple logging format.
       ),
     }),
   );
